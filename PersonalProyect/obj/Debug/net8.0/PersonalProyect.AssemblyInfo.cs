7bd4015c4133d39bef4155445b9b2d1b8124e967
sebastiancarrojas <sebastiancarrojas@gmail.com>
2025-12-08 18:53:42 -0500

    Imp vista Inicio, vistas parcialmente mejoradas Login, Register, Admin. Boton usuario implementado en Admin

--- PersonalProyect/obj/Debug/net8.0/PersonalProyect.AssemblyInfo.cs
+++ PersonalProyect/obj/Debug/net8.0/PersonalProyect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalProyect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27dadc9ee9a8f2854ff0f74544d848ef3cd6b44c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5589c693199ee483a6b72ac59e310c58ab223bba")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalProyect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalProyect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
